aef990132811d4ebe916b916f2151b271bf91f9d
MarcosFortuna <marcos.fortuna97@gmail.com>
2022-01-17 18:06:48 -0300

    reducer e custom hooks

---
+++ hooks/src/store/config.js
@@ -0,0 +1,13 @@
+import reducer from "./reducers"
+import {numberAdd2} from  './actions/number'
+const initialState = {
+    cart: [],
+    products: [],
+    user: null,
+    number: 0
+}
+
+export{
+    reducer,
+    initialState
+}
